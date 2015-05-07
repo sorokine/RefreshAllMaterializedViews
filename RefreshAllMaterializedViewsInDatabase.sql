32eb2f3912ae536d84fa7d33f1b23650ac177672
Jeff Casavant <jeff.casavant@gmail.com>
2015-05-07 14:37:08 -0400

    Match style of original

--- RefreshAllMaterializedViewsInDatabase.sql
+++ RefreshAllMaterializedViewsInDatabase.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION RefreshMatviewsConcurrently()
+CREATE OR REPLACE FUNCTION RefreshAllMaterializedViewsInDatabase()
 RETURNS INT AS $$
     DECLARE
         matview VARCHAR;
